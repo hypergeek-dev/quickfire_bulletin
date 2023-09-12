238dab254fece71f26604cacaa0aff6d6905b7a0
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-09-13 00:24:41 +0200

    Installing summernote

--- quickfire_bulletin/urls.py
+++ quickfire_bulletin/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from qfb_main import views  # Make sure to import views from your app
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('test/', views.test_page, name='test_page'),
+    path('summernote/', include('django_summernote.urls')),
 ]
