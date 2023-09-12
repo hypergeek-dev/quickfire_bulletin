a1def6ecc342488973116010b2a27e061450b4ea
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-09-12 16:24:45 +0200

    Updating views.py

--- qfb_main/views.py
+++ qfb_main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib import admin
+from django.urls import path
+from qfb_main import views  
 
-# Create your views here.
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('qfbmain/', views.home, name='qfbmain'),  
+    path('', views.home, name='home'),  
+]
