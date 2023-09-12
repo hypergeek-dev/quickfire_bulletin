1a49871869c366408fca9d3369721c07a00f4583
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-09-12 14:20:17 +0200

    Adding import dj_database_url

--- quickfire_bulletin/settings.py
+++ quickfire_bulletin/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -59,12 +60,16 @@ WSGI_APPLICATION = 'quickfire_bulletin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+ # DATABASES = {
+ #     'default': {
+ #         'ENGINE': 'django.db.backends.sqlite3',
+ #         'NAME': BASE_DIR / 'db.sqlite3',
+ #     }
+ # }
+    
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+ }
 
 
 # Password validation
