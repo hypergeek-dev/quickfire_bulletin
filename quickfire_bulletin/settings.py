1ce6bea5adb68476c15bf902d3f1fc8447cace3c
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-09-12 14:14:44 +0200

    Adding environmental file locally

--- quickfire_bulletin/settings.py
+++ quickfire_bulletin/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -6,17 +6,11 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
+DEBUG = os.environ.get('DEBUG') == 'True'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nqw9mm0j!8j*!%1us3bk((hxhatj^tr&%hh21h%m8a5m6@3dda'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
