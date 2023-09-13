b2ceb9d58a31161772fc2792a7ddb86080214c41
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-09-13 03:38:15 +0200

    Linking urls to templates

--- quickfire_bulletin/urls.py
+++ quickfire_bulletin/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
+    path("", views.PostList.as_view(), name="home"),
 ]
