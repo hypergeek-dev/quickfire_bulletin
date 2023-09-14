2b1e3c1aca49a03f12fb9020e207b2f2b948b0be
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-09-14 02:36:17 +0200

    Adding admin module to urls.py

--- quickfire_bulletin/urls.py
+++ quickfire_bulletin/urls.py
@@ -1,6 +1,8 @@
-from . import views
+from django.contrib import admin  # Import the admin module
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # Add this line for the admin interface
     path("", views.PostList.as_view(), name="home"),
 ]
