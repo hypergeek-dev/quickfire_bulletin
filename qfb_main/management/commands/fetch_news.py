a1ce354c041c2162fc5af1496a04ec1751ca90f4
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-09-20 00:03:12 +0200

    Error handling in fetch_news.api

--- qfb_main/management/commands/fetch_news.py
+++ qfb_main/management/commands/fetch_news.py
@@ -12,17 +12,22 @@ class Command(BaseCommand):
         Post.objects.filter(category='Tech', is_archived=False).update(is_archived=True)
 
         # Fetch new articles
-        url = f"https://newsapi.org/v2/top-headlines?country=us&category=technology&pageSize=5&apiKey={settings.NEWS_API_KEY}"
+        url = f"https://newsdata.io/api/1/news?apikey={settings.NEWS_API_KEY}"
         response = requests.get(url)
         data = response.json()
 
-        for article in data['articles']:
-            author, created = User.objects.get_or_create(username=article['source']['name'])
-            Post.objects.create(
-                title=article['title'],
-                content=article['description'],
-                author=author,
-                status=1,
-                category='Tech',
-                is_archived=False  # New articles are not archived
-            )
+        print(f"API Response: {data}")  # Debugging line to check what data contains
+
+        if response.status_code == 200:
+            for article in data.get('articles', []):
+                author, created = User.objects.get_or_create(username=article['source']['name'])
+                Post.objects.create(
+                    title=article['title'],
+                    content=article['description'],
+                    author=author,
+                    status=1,
+                    is_archived=False  # New articles are not archived
+                )
+        else:
+            print(f"Failed to fetch news: {response.status_code}")
+            print(f"Error details: {response.text}")  # Print the full response body for debugging
