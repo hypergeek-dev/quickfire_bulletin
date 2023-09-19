61ec8b12a2b0dcc58745022b9a2a0be48735eb6c
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-09-19 23:39:50 +0200

    Added category field to Post model

--- qfb_main/models.py
+++ qfb_main/models.py
@@ -2,9 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-
 STATUS = ((0, "Draft"), (1, "Published"))
-
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -20,6 +18,8 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
         User, related_name='blogpost_like', blank=True)
+    is_archived = models.BooleanField(default=False)  # New field for archiving
+    category = models.CharField(max_length=50, default='General')  # New field for category
 
     class Meta:
         ordering = ["-created_on"]
@@ -30,10 +30,8 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
-
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
